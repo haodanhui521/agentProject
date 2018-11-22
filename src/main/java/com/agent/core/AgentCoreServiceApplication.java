0d566eaeba8012aa1c20529f517562c951a8d5aa
danhui.hao@ftvalue.com <03007zys@>
2018-11-22 15:41:46 +0800

    'start'

---
+++ src/main/java/com/agent/core/AgentCoreServiceApplication.java
@@ -0,0 +1,12 @@
+package com.agent.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentCoreServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgentCoreServiceApplication.class, args);
+    }
+}
