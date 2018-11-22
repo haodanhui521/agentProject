7e013a5a08c0ec5cf167f7d37b683ae4eaa8fab8
haodanhui521 <45254880+haodanhui521@users.noreply.github.com>
2018-11-22 15:36:13 +0800

    start agent project

---
+++ agentCoreService/src/test/java/com/agent/core/AgentCoreServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.agent.core;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AgentCoreServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
